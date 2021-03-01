3adc7332ccc551bcc5d470b9890cf35b06adcb60
josetg01 <35702013+josetg01@users.noreply.github.com>
2021-03-01 16:29:03 +0100

    Update proyecto.sh

--- proyecto.sh
+++ proyecto.sh
@@ -5,3 +5,7 @@ wget --trust-server-name -O wps-office.deb
 sudo dpkg -i wps-office.deb
 wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/wps-full-fix-es_1.0-2019_all.deb/download -O wps-office-fix-es.deb
 sudo dpkg -i wps-office-fix-es.deb
+wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/opera-stable_64bits.deb/download -O opera-stable_64bits.deb
+sudo dpkg -i opera-stable_64bits.deb
+sudo apt update
+sudo apt install -y audacity rar

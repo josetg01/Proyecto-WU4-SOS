1f9baace654e2a93d8fc6006814f72a60d8ebf15
josetg01 <35702013+josetg01@users.noreply.github.com>
2021-03-01 11:55:04 +0100

    Update proyecto.sh

--- proyecto.sh
+++ proyecto.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/google-chrome.deb/download -O google-chrome.deb
+sudo dpkg -i google-chrome.deb
 wget --trust-server-name -O wps-office.deb
+sudo dpkg -i wps-office.deb
 wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/wps-full-fix-es_1.0-2019_all.deb/download -O wps-office-fix-es.deb
+sudo dpkg -i wps-office-fix-es.deb

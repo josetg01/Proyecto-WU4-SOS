b0847a8b8b2e4f857ae46074752e57066bdd6b92
josetg01 <35702013+josetg01@users.noreply.github.com>
2021-03-14 23:16:27 +0100

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/TEMAS/W9-Dark-29.zip/download -O W9.zip
+unzip W9.zip && sudo mv W9-Dark /usr/share/icons/
+wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/ICONOS/We10X-black.tar.xz/download -O We10X.tar.xz
+tar -Jxvf We10X.tar.xz && sudo mv We10X-black /usr/share/icons/
+sudo rm -r *

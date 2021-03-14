742314fff22ff1080b2f4672c29e24116c1b5cc7
josetg01 <35702013+josetg01@users.noreply.github.com>
2021-03-14 18:12:51 +0100

    Update and rename buil-iso.sh to build-iso.sh

--- buil-iso.sh
+++
@@ -1,1 +0,0 @@
-sudo wget https://github.com/josetg01/Proyecto-WU4-SOS/raw/main/xfce4-zorinmenulite-symbolic.svg -o /usr/share/icons/hicolor/symbolic/apps/xfce4-zorinmenulite-symbolic.svg
---
+++ build-iso.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/google-chrome.deb/download -O google-chrome.deb
+sudo dpkg -i google-chrome.deb
+wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/wps-office.deb/download -O wps-office.deb
+sudo dpkg -i wps-office.deb
+wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/wps-full-fix-es_1.0-2019_all.deb/download -O wps-office-fix-es.deb
+sudo dpkg -i wps-office-fix-es.deb
+wget --trust-server-name https://sourceforge.net/projects/proyecto-wu4-sos/files/SOFTWARE/opera-stable_64bits.deb/download -O opera-stable_64bits.deb
+sudo dpkg -i opera-stable_64bits.deb
+sudo apt update
+sudo apt install -y audacity rar vlc
+sudo wget https://github.com/josetg01/Proyecto-WU4-SOS/raw/main/xfce4-zorinmenulite-symbolic.svg -o /usr/share/icons/hicolor/symbolic/apps/xfce4-zorinmenulite-symbolic.svg
+sudo apt purge -y gnome-mines aisleriot gnome-mahjongg quadrapassel gnome-sudoku gimp pitivi cheese xfburn libreoffice-base parole
+sudo apt autoremove -y
+sudo apt upgrade -y
